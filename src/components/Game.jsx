4c31d5e73b218a00ad641e4f0b67cdb474018781
Rodrigo Poblete <pobletedev@gmail.com>
2022-11-24 23:24:02 -0300

    añadiendo custom hook

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,51 +1,18 @@
 import { useEffect, useRef, useState } from "react"
-import { gameData } from "../data/gameData"
+import { useGameFunctions } from "../hooks/useGameFunctions"
 import { Cards } from "./index"
 
 export const Game = () => {
 
     //TODO: Corregir errores:
 
-    const animateContainer = useRef()
-    const [startedGame, setstartedGame] = useState(false)
-    const [points, setpoints] = useState(0)
-    const [dataState, setdataState] = useState(gameData)
-    const [currentPosition, setcurrentPosition] = useState()
-
-    const onHandleClick = (index) => {
-        if (!startedGame) return;
-        setcurrentPosition(index);
-        dataState[index].active = true;
-        isEqual(index);
-    }
-
-    const isEqual = (index) => {
-        if (dataState[index].image_url == dataState[currentPosition].image_url) {
-            setcurrentPosition(-1);
-            setdataState([...dataState]);
-            setpoints(points + 100);
-        } else {
-            setTimeout(() => {
-                setcurrentPosition(-1)
-                dataState[index].active = false;
-                dataState[currentPosition].active = false;
-                setdataState([...dataState]);
-            }, 500);
-        }
-    }
-
-    const onStartGame = () => {
-        setTimeout(() => {
-            dataState.sort(() => Math.random() * 100)
-            dataState.map(e => e.active = false)
-            setdataState([...dataState])
-        }, 1000);
-        dataState.sort(() => Math.random() - 3)
-        dataState.map(e => e.active = true)
-        setdataState([...dataState])
-        setstartedGame(true);
-    }
-
+    const {
+        dataState, 
+        points,
+        animateContainer,
+        onHandleClick,
+        onStartGame  
+    } = useGameFunctions();
 
     return (
         <>
